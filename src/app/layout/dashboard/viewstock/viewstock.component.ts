b15695403759db10ed7605c29c7afbcebd585c61
Rojan <roseonmhr@gmail.com>
2021-07-04 08:55:44 +0545

    typed fix doc dat as object

--- src/app/layout/dashboard/viewstock/viewstock.component.ts
+++ src/app/layout/dashboard/viewstock/viewstock.component.ts
@@ -1,33 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { MedicineService } from 'src/app/shared/medicine.service';
-import { ToastrService } from 'ngx-toastr';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Medicine } from 'src/app/shared/medicine.model';
+import { Component, OnInit } from "@angular/core";
+import { MedicineService } from "src/app/shared/medicine.service";
+import { ToastrService } from "ngx-toastr";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Medicine } from "src/app/shared/medicine.model";
 
 @Component({
-  selector: 'app-viewstock',
-  templateUrl: './viewstock.component.html',
-  styleUrls: ['./viewstock.component.css']
+  selector: "app-viewstock",
+  templateUrl: "./viewstock.component.html",
+  styleUrls: ["./viewstock.component.css"],
 })
 export class ViewstockComponent implements OnInit {
   list: Medicine[];
   p: number = 1;
-  userFilter: any = { medicineName: '' };
+  userFilter: any = { medicineName: "" };
   constructor(
     private service: MedicineService,
     private toastr: ToastrService,
     private firestore: AngularFirestore
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.service.getApproveMedicine().subscribe(actionArray => {
-      this.list = actionArray.map(item => {
+    this.service.getApproveMedicine().subscribe((actionArray) => {
+      this.list = actionArray.map((item) => {
         return {
           id: item.payload.doc.id,
-          ...item.payload.doc.data()
+          ...(item.payload.doc.data() as Object),
         } as Medicine;
-      })
-    })
+      });
+    });
   }
-
 }
--- src/app/layout/dashboard/viewuser/viewuser.component.ts
+++ src/app/layout/dashboard/viewuser/viewuser.component.ts
@@ -1,35 +1,34 @@
-import { User } from './../../../shared/user.model';
-import { UserService } from './../../../shared/user.service';
-import { Component, OnInit } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from "./../../../shared/user.model";
+import { UserService } from "./../../../shared/user.service";
+import { Component, OnInit } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
-  selector: 'app-viewuser',
-  templateUrl: './viewuser.component.html',
-  styleUrls: ['./viewuser.component.css']
+  selector: "app-viewuser",
+  templateUrl: "./viewuser.component.html",
+  styleUrls: ["./viewuser.component.css"],
 })
 export class ViewuserComponent implements OnInit {
   list: User[];
   p: number = 1;
-  userFilter: any = { fullName: '' };
+  userFilter: any = { fullName: "" };
   constructor(
     private service: UserService,
     private toastr: ToastrService,
     private firestore: AngularFirestore
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.service.getUser().subscribe(actionArray => {
-      this.list = actionArray.map(item => {
+    this.service.getUser().subscribe((actionArray) => {
+      this.list = actionArray.map((item) => {
         return {
           id: item.payload.doc.id,
-          ...item.payload.doc.data()
+          ...(item.payload.doc.data() as Object),
         } as User;
-      })
+      });
       const numberUser = this.list.length;
       console.log("The Total Numer of Users" + numberUser);
-    })
+    });
   }
-
 }
